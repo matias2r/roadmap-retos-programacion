1c45cc927826816181a8839d9ec15ee595ccc8d8
Matias Espinoza <51390169+matias2r@users.noreply.github.com>
2024-05-11 23:19:45 -0400

    #01 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/matias2r.js
@@ -0,0 +1,30 @@
+/* Sitio web oficial JavaScript https://developer.mozilla.org/es/docs/Web/JavaScript*/
+
+
+/* Sintaxis 
+para crear comentarios 
+Multi linea */
+
+// Sintaxtis para crear comentarios de 1 sola linea
+
+let edad = 26;
+const a = 26;
+
+// String
+let string = "Esto es un String";
+
+// Number
+let Number = 26;
+
+// Big Int
+let bigInt = bigInt(9007199254740991);
+
+// Boolean
+let BooleanTrue = true;
+let BooleanFalse = false;
+
+// Symbol
+let sym = Symbol("Simbolo");
+
+
+console.log("¡Hola, JavaScript!");
